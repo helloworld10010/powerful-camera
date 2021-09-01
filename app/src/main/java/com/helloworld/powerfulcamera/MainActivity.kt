958b2a0edb2f768c64a64ada338d2e0a4f8694c2
gjj <guojam@outlook.com>
2021-09-01 16:28:40 +0800

    0.7.1

--- app/src/main/java/com/helloworld/powerfulcamera/MainActivity.kt
+++ app/src/main/java/com/helloworld/powerfulcamera/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.helloworld.powerfulcamera.databinding.ActivityMainSampleBinding
-import com.helloworld.powerfullcamera.CouousCameraActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     binding.jump.setOnClickListener {
-      startActivity(Intent(this,CouousCameraActivity::class.java))
+
     }
   }
 }
